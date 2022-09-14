6fa6ed506e8c42132e1832c2c686540dfa19fa0b
RVM <113094584+Rafavermar@users.noreply.github.com>
2022-09-14 21:10:09 +0200

    Delete script avotable 2.sql

--- script avotable 2.sql
+++
@@ -1,8 +0,0 @@
-SET SQL_SAFE_UPDATES = 0;
-
-SET @@global.event_scheduler = 0;
-
-SELECT flujo, año, mes, avotable.cod_pais, provincia, euros, cod_taric, nivel_taric, kilogramos
-FROM avodb.avotable
-WHERE cod_taric LIKE '8044%' AND nivel_taric IN (5,3) AND  flujo = 'I' AND cod_pais = 41;
-
